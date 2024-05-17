e432a6ba76d8166eeca18e157f2d74b26bdefada
M.elsayed <M.elsayed@DXBPP-PROG06>
2024-05-17 09:27:22 +0400

    Adding a test comment

--- ExchangeApp/Helper/AppointmentRequest.cs
+++ ExchangeApp/Helper/AppointmentRequest.cs
@@ -2,6 +2,7 @@
 {
     public class AppointmentRequest
     {
+        // test comment
         public string Subject { get; set; }
         public string Location { get; set; }
         public DateTime StartTime { get; set; }
